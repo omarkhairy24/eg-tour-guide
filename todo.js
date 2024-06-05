0b4c3eec6cfcd34e383053a7e67b58d80e597c93
Amr mohamed <amrmohamd229@gmail.com>
2024-06-05 19:48:00 +0300

    damietta added

--- todo.js
+++ todo.js
@@ -1,5 +1,8 @@
 /*
-1- damieeta place add to db
-2- add vrs to some places
-3- monumenta in artifacs we collected in db
+1- damieeta place add to db✅
+2- add vrs to some places .. me
+3- monumenta in artifacs we collected in db .. omar
+4- recommendation system .. omar
+5- ar model .. omar
+6- vr model of ras el bar ..me
 */
