8b87a4e49371bcb8e01f054f0be58e702a8c8eb9
Amr mohamed <amrmohamd229@gmail.com>
2024-06-20 00:02:33 +0300

    fav places error

--- models/favoritePlace.js
+++ models/favoritePlace.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose');
 
 const favSchema = new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.ObjectId,
-        ref:'User',
-        required:true
-    },
-    place:{
-        type:mongoose.Schema.ObjectId,
-        ref:'Places'
-    },
-    artifacs:{
-        type:mongoose.Schema.ObjectId,
-        ref:'Artifacs'
-    },
-    tour:{
-        type:mongoose.Schema.ObjectId,
-        ref:'Tours'
-    }
+	user: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	place: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'Places',
+	},
+	artifacs: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'Artifacs',
+	},
+	tour: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'Tours',
+	},
 });
 
-favSchema.index({user:1,place:1,artifacs:1},{unique:true});
+favSchema.index({ user: 1, place: 1, artifacs: 1, tour: 1 }, { unique: true });
 
-favSchema.pre(/^find/,function(next){
-    this.populate('place artifacs');
-    next();
+favSchema.pre(/^find/, function (next) {
+	this.populate('place artifacs');
+	next();
 });
 
-module.exports = mongoose.model('Favorite',favSchema);
+module.exports = mongoose.model('Favorite', favSchema);
